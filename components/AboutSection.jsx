8e83e56e1f519435f8d64cf27113c42ef3b44974
Andy Park <chanhyup@andrew.cmu.edu>
2023-05-22 18:42:25 -0400

    Add about me

--- components/AboutSection.jsx
+++ components/AboutSection.jsx
@@ -9,8 +9,21 @@ const HomeSection = ({ active }) => {
         style={{ display: active ? "block" : "none" }}
       >
         <hr id="outer-divider" />
-        
-        <hr id="outer-divider"/>
+
+        <p id="about">About</p>
+        <p id="about__text">
+          <span className="p">
+            Hello! I am a second year Ph.D. candidate in Physics at McWilliams
+            Center for Cosmology, Carnegie Mellon University, where I am
+            advisded by Rachel Mandelbaum studying observational cosmology.
+          </span>
+          <span className="p">
+            Prior to graduate school, I recieved my B.S. in Mathematics and Physics
+            (specialization in Astrophysics) at the Univeristy of California, Irvine.
+          </span>
+        </p>
+
+        <hr id="outer-divider" />
       </section>
     </>
   );
