af77aa4eafd5d543a6bed15762ee140cbe771ed7
Andy Park <chanhyup@andrew.cmu.edu>
2023-05-22 19:05:05 -0400

    Add contact section

---
+++ components/ContactSection.jsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+const HomeSection = ({ active }) => {
+  return (
+    <>
+      <section
+        id="home-section"
+        className={`${active ? "active" : "inactive"}`}
+        style={{ display: active ? "block" : "none" }}
+      >
+        <hr id="outer-divider" />
+
+        <hr id="outer-divider" />
+      </section>
+    </>
+  );
+};
+
+export default HomeSection;
