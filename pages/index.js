b342d761a31f9cfbfc73a981039b1502adddb239
Andy Park <chanhyup@andrew.cmu.edu>
2023-05-22 11:29:08 -0400

    Add image

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
 import Layout from "@/components/Layout";
 import NavBar from "@/components/Navbar";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <Layout>
       <NavBar />
-      <hr id="outer-divider"/>
+      <hr id="outer-divider" />
+
+      <section id="home-section" className="active">
+        <div className="image">
+          <span className="frame">
+            <img src="/ParkAndy.jpg"/>
+          </span>
+        </div>
+      </section>
     </Layout>
-  )
+  );
 }
