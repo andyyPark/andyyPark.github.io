37b745b9a2cba49a8558c2b570e7dcd4fce7a132
Andy Park <chanhyup@andrew.cmu.edu>
2023-06-01 11:32:25 -0400

    Add title

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,10 +2,14 @@ import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
   return (
-    <div id="wrapper">
+    <div>
+      <title>Andy Park</title>
+      <meta name="description" content="My homepage"/>
+      <div id="wrapper">
         <div id="main">
-        <div className="inner">{children}</div>
+          <div className="inner">{children}</div>
         </div>
+      </div>
     </div>
   );
 }
