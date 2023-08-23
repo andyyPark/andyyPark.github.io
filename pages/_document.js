4bf5a7c0f6e45e2aece9a740f251dc84a938e29e
Andy Park <chanhyup@andrew.cmu.edu>
2023-08-23 16:35:44 -0400

    Add metadata

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,10 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Welcome to my personal website. I am Andy Park, a physics PhD student at Carnegie Mellon University. Learn about my research and academic interests."
+        />
       </Head>
       <body>
         <Main />
